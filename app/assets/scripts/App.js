0ecb642cd827b0bb7a46280ff9dac8e4ee9f107e
vitordev1435 <vitordev1435@gmail.com>
2021-01-31 15:59:27 +0000

    new files

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,14 +1,8 @@
 import '../styles/styles.css'
-<<<<<<< HEAD
-import MobileMenu from './modules/MobileMenu';
-=======
 import MobileMenu from './modules/MobileMenu'
->>>>>>> our-features
 
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept()
 }
-
-
